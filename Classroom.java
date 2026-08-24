bf75a01132cc518d9a8a0eb7174e0c8e499d2032
RishiMohanty <rishi_mohanty@outlook.com>
2026-08-24 09:41:22 -0700

    added wav

--- Classroom.java
+++ Classroom.java
@@ -174,13 +174,11 @@ public class Classroom extends World
  // Each student needs to create their specific instance following the KilgoreTrout example.
  // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
  // and then clicking on the inspect text
-        KilgoreTrout kilgoretrout = new KilgoreTrout();
-        addObject(kilgoretrout,2,6);
-        kilgoretrout.assignSeat();
+
         
         
         RishiMohanty rishimohanty = new RishiMohanty();
-        addObject(rishimohanty, 2, 7);
+        addObject(rishimohanty, 2, 6);
         rishimohanty.assignSeat();
  
         
