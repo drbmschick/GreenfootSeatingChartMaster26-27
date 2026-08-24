5d424c1a005093087921bfbc10ea5c3541c35d27
SunayKaushal <sunaykaushal@gmail.com>
2026-08-24 10:12:33 -0700

    Replace KilgoreTrout with specific student instances

--- Classroom.java
+++ Classroom.java
@@ -125,6 +125,7 @@ public class Classroom extends World
         
         TeacherDesk teacherdesk = new TeacherDesk();
         addObject(teacherdesk,1,11);
+        
         studentdesk31.setDeskGroup(1);
         studentdesk29.setDeskGroup(1);
         studentdesk30.setDeskGroup(1);
@@ -174,16 +175,30 @@ public class Classroom extends World
  // Each student needs to create their specific instance following the KilgoreTrout example.
  // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
  // and then clicking on the inspect text
-        KilgoreTrout kilgoretrout = new KilgoreTrout();
-        addObject(kilgoretrout,2,6);
-        kilgoretrout.assignSeat();
+
+
         
+        RishiMohanty rishimohanty = new RishiMohanty();
+        addObject(rishimohanty, 2, 6);
+        rishimohanty.assignSeat();
+ 
+        
+        DavidLin davidlin = new DavidLin();
+        addObject(davidlin,8,10);
+        davidlin.assignSeat();
+  
+        BrianSchick brianschick = new BrianSchick();
+        addObject(brianschick,1,11);
+        brianschick.assignSeat();
+        ShivamPrasad shivamprasad = new ShivamPrasad();
+        addObject(shivamprasad,2,7);
+        shivamprasad.assignSeat();
         SunayKaushal sunay = new SunayKaushal();
         addObject(sunay, 5,6);
         sunay.assignSeat();
-  
+
     }
-    
+ 
     public List<Student> getAllStudents(){
        List<Student> s = getObjects(Student.class);  
        return s;
