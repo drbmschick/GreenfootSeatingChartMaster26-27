0d59dae9c484047311bd220dd415c20963a74848
Samarth Gaggar <samarthgaggar2010@gmail.com>
2026-08-24 09:59:46 -0700

    added samarth files

--- Classroom.java
+++ Classroom.java
@@ -174,9 +174,10 @@ public class Classroom extends World
  // Each student needs to create their specific instance following the KilgoreTrout example.
  // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
  // and then clicking on the inspect text
-        KilgoreTrout kilgoretrout = new KilgoreTrout();
-        addObject(kilgoretrout,2,6);
-        kilgoretrout.assignSeat();
+     
+        SamarthGaggar samarthgaggar = new SamarthGaggar();
+        addObject(samarthgaggar,3,6);
+        samarthgaggar.assignSeat();
   
     }
     
